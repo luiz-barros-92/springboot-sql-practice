7409db21c7ea7f63327b0cfd068e1ff5d02ca215
Luiz Barros <lck.barros@gmail.com>
2025-07-26 19:17:12 -0300

    first commit

---
+++ uri2603/src/main/java/com/luizbarros/uri2603/Uri2603Application.java
@@ -0,0 +1,13 @@
+package com.luizbarros.uri2603;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2603Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2603Application.class, args);
+	}
+
+}
---
+++ uri2603/src/test/java/com/luizbarros/uri2603/Uri2603ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizbarros.uri2603;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2603ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
