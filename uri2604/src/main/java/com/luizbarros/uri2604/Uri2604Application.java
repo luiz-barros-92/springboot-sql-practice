e2e108c9633a91c51823e1b3ab8dcc4115b4feac
Luiz <lck.barros@gmail.com>
2025-07-29 12:32:50 -0300

    feat(uri2604): first commit

---
+++ uri2604/src/main/java/com/luizbarros/uri2604/Uri2604Application.java
@@ -0,0 +1,13 @@
+package com.luizbarros.uri2604;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2604Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2604Application.class, args);
+	}
+
+}
---
+++ uri2604/src/test/java/com/luizbarros/uri2604/Uri2604ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizbarros.uri2604;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2604ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
