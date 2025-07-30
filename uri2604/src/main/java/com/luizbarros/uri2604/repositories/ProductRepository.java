c650887fb7413b0d2b7f77439c3a0ae71a402bd2
Luiz Barros <lck.barros@gmail.com>
2025-07-30 20:05:08 -0300

    feat(uri2604): create ProductRepository with the query

---
+++ uri2604/src/main/java/com/luizbarros/uri2604/repositories/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.luizbarros.uri2604.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.luizbarros.uri2604.entities.Product;
+import com.luizbarros.uri2604.projections.ProductMinProjection;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	@Query(nativeQuery = true, value = "SELECT id, name "
+			+ "FROM products "
+			+ "WHERE price < :low OR price > :high;")
+	List<ProductMinProjection> search(Integer low, Integer high);	
+}
