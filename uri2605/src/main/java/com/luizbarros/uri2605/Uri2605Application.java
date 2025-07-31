329e81cf63409bdd8b01278dfc4d599064ddc934
Luiz <lck.barros@gmail.com>
2025-07-31 12:58:38 -0300

    feat(uri2605): first commit

---
+++ uri2605/src/main/java/com/luizbarros/uri2605/Uri2605Application.java
@@ -0,0 +1,13 @@
+package com.luizbarros.uri2605;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2605Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2605Application.class, args);
+	}
+
+}
---
+++ uri2605/src/test/java/com/luizbarros/uri2605/Uri2605ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizbarros.uri2605;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2605ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
