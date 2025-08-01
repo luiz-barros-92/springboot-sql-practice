8aef4ccb9b056a1ed1de5be2eadc490bc1ce345d
Luiz Barros <lck.barros@gmail.com>
2025-07-31 21:21:16 -0300

    feat(uri2605): create entity 'Category'

---
+++ uri2605/src/main/java/com/luizbarros/uri2605/entities/Category.java
@@ -0,0 +1,43 @@
+package com.luizbarros.uri2605.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+	@Id
+	private Long id;
+	private String name;
+	
+	public Category() {
+	}
+
+	public Category(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}	
+}
