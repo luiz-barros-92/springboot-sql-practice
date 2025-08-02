6af93b1ebecd73f2599a128e793c8ee42955122c
Luiz Barros <lck.barros@gmail.com>
2025-08-02 18:25:03 -0300

    fix(uri2605): insert annotations correctly

--- uri2605/src/main/java/com/luizbarros/uri2605/entities/Provider.java
+++ uri2605/src/main/java/com/luizbarros/uri2605/entities/Provider.java
@@ -1,7 +1,17 @@
 package com.luizbarros.uri2605.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "providers")
 public class Provider {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String street;
