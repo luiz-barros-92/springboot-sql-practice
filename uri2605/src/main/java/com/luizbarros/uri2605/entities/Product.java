8f75cfdb0e56f98f29d0d6b6fe1dcedd6864b655
Luiz Barros <lck.barros@gmail.com>
2025-08-02 18:50:40 -0300

    fix(uri2605): sql refactor and missing @Id annotation

--- uri2605/src/main/java/com/luizbarros/uri2605/entities/Product.java
+++ uri2605/src/main/java/com/luizbarros/uri2605/entities/Product.java
@@ -1,6 +1,7 @@
 package com.luizbarros.uri2605.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -9,6 +10,7 @@ import jakarta.persistence.Table;
 @Table(name = "products")
 public class Product {
 
+	@Id
 	private Long id;
 	private String name;
 	private Long amount;
