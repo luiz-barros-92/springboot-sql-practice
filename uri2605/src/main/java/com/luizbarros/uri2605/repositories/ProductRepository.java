401e4a84a7f7cd31347934ed57f7588de558fd9a
Luiz Barros <lck.barros@gmail.com>
2025-08-03 18:17:12 -0300

    feat(uri2605): create ProductRepository
    test(uri2605): set the application properties to dev

---
+++ uri2605/src/main/java/com/luizbarros/uri2605/repositories/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.luizbarros.uri2605.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.luizbarros.uri2605.entities.Product;
+import com.luizbarros.uri2605.projections.ProductProviderMinProjection;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	@Query(nativeQuery = true, value = "SELECT P.name, PR.name "
+			+ "FROM products P "
+			+ "JOIN providers PR ON P.id_providers = PR.id "
+			+ "WHERE P.id_categories = :categoryId")
+	List<ProductProviderMinProjection> search(Long categoryId);
+}
