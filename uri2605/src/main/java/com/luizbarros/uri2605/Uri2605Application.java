376380ded643a2c788eb539429f69eeb320a6fd8
Luiz Barros <lck.barros@gmail.com>
2025-08-03 18:39:03 -0300

    feat(uri2605): set the main application to run the query in console
    fix(uri2605): refactor the query in ProductRepository to match the methods in ProductProviderMinProjection

--- uri2605/src/main/java/com/luizbarros/uri2605/Uri2605Application.java
+++ uri2605/src/main/java/com/luizbarros/uri2605/Uri2605Application.java
@@ -1,13 +1,35 @@
 package com.luizbarros.uri2605;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2605Application {
+import com.luizbarros.uri2605.dto.ProductMinDTO;
+import com.luizbarros.uri2605.projections.ProductProviderMinProjection;
+import com.luizbarros.uri2605.repositories.ProductRepository;
 
+@SpringBootApplication
+public class Uri2605Application implements CommandLineRunner{
+
+	@Autowired
+	private ProductRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2605Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<ProductProviderMinProjection> list = repository.search(6L);
+		List<ProductMinDTO> result = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
+		
+		for (ProductMinDTO obj : result) {
+			System.out.println(obj);
+		}
 	}
 
 }
--- uri2605/src/main/java/com/luizbarros/uri2605/repositories/ProductRepository.java
+++ uri2605/src/main/java/com/luizbarros/uri2605/repositories/ProductRepository.java
@@ -10,7 +10,7 @@ import com.luizbarros.uri2605.projections.ProductProviderMinProjection;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
-	@Query(nativeQuery = true, value = "SELECT P.name, PR.name "
+	@Query(nativeQuery = true, value = "SELECT P.name AS nameProduct, PR.name AS nameProvider "
 			+ "FROM products P "
 			+ "JOIN providers PR ON P.id_providers = PR.id "
 			+ "WHERE P.id_categories = :categoryId")
