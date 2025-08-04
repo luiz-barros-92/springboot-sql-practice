09223bce41f825fd75423539b148a6d14c62467a
Luiz Barros <lck.barros@gmail.com>
2025-08-04 18:56:52 -0300

    feat(uri2613): first commit

---
+++ uri2613/src/test/java/com/luizbarros/uri2613/Uri2613ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizbarros.uri2613;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri2613ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
