7a8e60ea8f657e43bb2b4a2cc7195b2532567bfc
Luiz Barros <lck.barros@gmail.com>
2025-08-06 20:32:31 -0300

    test(uri2613): db seed

---
+++ uri2613/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO prices (id , categorie, value) VALUES (1, 'Releases', 3.50);
+INSERT INTO prices (id , categorie, value) VALUES (2, 'Bronze Seal', 2.00);
+INSERT INTO prices (id , categorie, value) VALUES (3, 'Silver Seal', 2.50);
+INSERT INTO prices (id , categorie, value) VALUES (4, 'Gold Seal', 3.00);
+INSERT INTO prices (id , categorie, value) VALUES (5, 'Promotion', 1.50);
+  
+INSERT INTO movies (id, name, id_prices) VALUES (1, 'Batman', 3);
+INSERT INTO movies (id, name, id_prices) VALUES (2,	'The Battle of the Dark River',	3);
+INSERT INTO movies (id, name, id_prices) VALUES (3,	'White Duck', 5);
+INSERT INTO movies (id, name, id_prices) VALUES (4,	'Breaking Barriers', 4);
+INSERT INTO movies (id, name, id_prices) VALUES (5,	'The Two Hours', 2);
