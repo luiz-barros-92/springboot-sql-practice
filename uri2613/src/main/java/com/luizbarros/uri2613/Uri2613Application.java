3ec64b8cbd46f997a6cd7b696839bcaf259a7e9d
Luiz Barros <lck.barros@gmail.com>
2025-08-08 20:46:46 -0300

    feat(uri2613): feat: finish the main application

--- uri2613/src/main/java/com/luizbarros/uri2613/Uri2613Application.java
+++ uri2613/src/main/java/com/luizbarros/uri2613/Uri2613Application.java
@@ -1,13 +1,33 @@
 package com.luizbarros.uri2613;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Uri2613Application {
+import com.luizbarros.uri2613.dto.MovieMinDTO;
+import com.luizbarros.uri2613.projections.MovieProjection;
+import com.luizbarros.uri2613.repositories.MovieRepository;
 
+@SpringBootApplication
+public class Uri2613Application implements CommandLineRunner{
+
+	private MovieRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2613Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<MovieProjection> list = repository.search(2L);
+		List<MovieMinDTO> result = list.stream().map(x -> new MovieMinDTO(x)).collect(Collectors.toList());
+		
+		for (MovieMinDTO obj : result) {
+			System.out.println(obj);
+		}
 	}
 
 }
